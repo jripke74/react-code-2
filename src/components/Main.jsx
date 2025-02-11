f33f600785254f1178e9dc32b94a9341cc2c733b
Jeff Ripke <jripke74@gmail.com>
2025-02-11 13:35:59 -0800

    add css, html

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,16 @@
 export default function Main() {
   return (
-    <h1>Main component here</h1>
+    <>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+
   );
 }
